6c04846e14f5f20234b0b3f6c4b74f922e113c60
buhe <bugu1986@126.com>
2022-01-17 18:56:33 +0800

    Updates

---
+++ precache-manifest.2496aead3d9185ee11bdbe01f7e16357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57570414f87850456b1",
+    "url": "/bugu-lang-wasm2/e441547007c8a6b9b166.module.wasm"
+  },
+  {
+    "revision": "414de2b1da7e6b29dc5b600b0456ff74",
+    "url": "/bugu-lang-wasm2/index.html"
+  },
+  {
+    "revision": "fca8c40198db99365922",
+    "url": "/bugu-lang-wasm2/static/css/2.0c770f5e.chunk.css"
+  },
+  {
+    "revision": "6b8efdcf58da807fb5d6",
+    "url": "/bugu-lang-wasm2/static/css/main.a31bcee3.chunk.css"
+  },
+  {
+    "revision": "fca8c40198db99365922",
+    "url": "/bugu-lang-wasm2/static/js/2.b97e8107.chunk.js"
+  },
+  {
+    "revision": "e57570414f87850456b1",
+    "url": "/bugu-lang-wasm2/static/js/3.2881a011.chunk.js"
+  },
+  {
+    "revision": "6b8efdcf58da807fb5d6",
+    "url": "/bugu-lang-wasm2/static/js/main.4b89a5bd.chunk.js"
+  },
+  {
+    "revision": "fc72bc5a72a1d42b0a26",
+    "url": "/bugu-lang-wasm2/static/js/runtime-main.a56ac36b.js"
+  }
+]);
